95190b13cf558d20a7b71bd442ecd772f7c95dbd
grace0950 <pinyan0950@gmail.com>
2024-03-28 17:09:31 +0800

    Add jettonTransfer function and implement deposit functionality

--- tests/MasterChef.spec.ts
+++ tests/MasterChef.spec.ts
@@ -1,20 +1,61 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { Address, beginCell, toNano } from '@ton/core';
 import { MasterChef } from '../wrappers/MasterChef';
 import { MiniChef } from '../wrappers/MiniChef';
 import { JettonWalletUSDT } from '../wrappers/JettonWallet';
 import { JettonMasterUSDT } from '../wrappers/JettonMaster';
 import '@ton/test-utils';
+import { Deploy } from '../build/Jetton/tact_JettonWalletUSDT';
 
 describe('PoolFactory', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
+    let user: SandboxContract<TreasuryContract>;
     let masterChef: SandboxContract<MasterChef>;
     let usdt: SandboxContract<JettonMasterUSDT>;
+
+    async function jettonTransfer(
+        usdt: SandboxContract<JettonMasterUSDT>,
+        user: SandboxContract<TreasuryContract>,
+        masterChef: SandboxContract<MasterChef>,
+    ) {
+        await usdt.send(user.getSender(), { value: toNano('1') }, 'Mint:1');
+        const userJettonWallet = blockchain.openContract(await JettonWalletUSDT.fromInit(user.address, usdt.address));
+        return await userJettonWallet.send(
+            user.getSender(),
+            { value: toNano('1.1') },
+            {
+                $$type: 'JettonTransfer',
+                query_id: 0n,
+                amount: 1n * 10n ** 6n,
+                destination: masterChef.address,
+                response_destination: user.address,
+                custom_payload: null,
+                forward_ton_amount: toNano('1'),
+                forward_payload: beginCell().endCell(),
+            },
+        );
+    }
+
+    async function addPool() {
+        const masterChefJettonWallet = blockchain.openContract(
+            await JettonWalletUSDT.fromInit(masterChef.address, usdt.address),
+        );
+        return await masterChef.send(
+            deployer.getSender(),
+            { value: toNano('0.05') },
+            {
+                $$type: 'AddPool',
+                lpTokenAddress: masterChefJettonWallet.address,
+                allocPoint: 100n,
+            },
+        );
+    }
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury('deployer');
+        user = await blockchain.treasury('user');
         masterChef = blockchain.openContract(await MasterChef.fromInit(deployer.address, 100n));
         usdt = blockchain.openContract(await JettonMasterUSDT.fromInit(deployer.address, beginCell().endCell()));
 
@@ -43,12 +84,15 @@ describe('PoolFactory', () => {
     });
 
     it('Should add pool', async () => {
+        const masterChefJettonWallet = blockchain.openContract(
+            await JettonWalletUSDT.fromInit(masterChef.address, usdt.address),
+        );
         const addPoolResult = await masterChef.send(
             deployer.getSender(),
             { value: toNano('0.05') },
             {
                 $$type: 'AddPool',
-                lpTokenAddress: usdt.address,
+                lpTokenAddress: masterChefJettonWallet.address,
                 allocPoint: 100n,
             },
         );
@@ -59,28 +103,18 @@ describe('PoolFactory', () => {
             success: true,
             op: 1266490084,
         });
-
-        await usdt.send(deployer.getSender(), { value: toNano('0.05') }, 'Mint:1');
-        const deployerJettonWallet = blockchain.openContract(
-            await JettonWalletUSDT.fromInit(deployer.address, usdt.address),
-        );
-        deployerJettonWallet.send(
-            deployer.getSender(),
-            { value: toNano('1.1') },
-            {
-                $$type: 'JettonTransfer',
-                query_id: 1n,
-                amount: 1n * 10n ** 6n,
-                destination: masterChef.address,
-                response_destination: masterChef.address,
-                custom_payload: null,
-                forward_ton_amount: toNano('1'),
-                forward_payload: beginCell().endCell(),
-            },
-        );
     });
 
-    it('Should deposit', async () => {});
+    it('Should deposit', async () => {
+        await addPool();
+        const jettonTransferResult = await jettonTransfer(usdt, user, masterChef);
+        printTransactionFees(jettonTransferResult.transactions);
+        expect(jettonTransferResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: masterChef.address,
+            success: true,
+        });
+    });
 
     it('Should deposit and harvest', async () => {});
 
