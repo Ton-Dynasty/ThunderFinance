3ff6f55f76a50c8083d218828c7f38122bd16f7a
alan890104 <alan890104@gmail.com>
2024-04-19 23:18:26 +0800

    fix: add min gas fee to restrict refund action

--- scripts/buildTonMasterChef.ts
+++ scripts/buildTonMasterChef.ts
@@ -8,14 +8,14 @@ export async function run(provider: NetworkProvider) {
     const kitchen = provider.open(Kitchen.fromAddress(Address.parse(deployment.Kitchen)));
     const seed = BigInt(`0x${beginCell().storeUint(Date.now(), 64).endCell().hash().toString('hex')}`);
     const masterchefAddress = await kitchen.getGetJettonMasterChefAddress(provider.sender().address!!, seed);
-    const totalReward = 1n * 10n ** 10n;
+    const totalReward = toNano("0.01")
     let sentAmount = (totalReward * 1003n) / 1000n;
     const deadline = BigInt(Math.floor(Date.now() / 1000) + 60 * 60);
-
+    const startTime = BigInt(Math.floor(Date.now() / 1000)) + 10n;
     await kitchen.send(
         provider.sender(),
         {
-            value: sentAmount + toNano('1'),
+            value: sentAmount + toNano('0.1'),
         },
         {
             $$type: 'BuildTonMasterChef',
@@ -23,7 +23,7 @@ export async function run(provider: NetworkProvider) {
             seed: seed,
             metaData: beginCell().storeStringTail('httpppp').endCell(),
             deadline: deadline,
-            startTime: BigInt(Math.floor(Date.now() / 1000)) + 10n,
+            startTime: startTime,
             totalReward: totalReward,
         },
     );
