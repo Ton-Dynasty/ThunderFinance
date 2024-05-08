4df6d64c4387e822db90ce6441ecc77b5ea526ff
ipromise2324 <168allen55@gmail.com>
2024-05-08 14:09:41 +0800

    feat: dcrease fee

--- scripts/buildTonMasterChef.ts
+++ scripts/buildTonMasterChef.ts
@@ -8,7 +8,7 @@ export async function run(provider: NetworkProvider) {
     const kitchen = provider.open(Kitchen.fromAddress(Address.parse(deployment.Kitchen)));
     const seed = BigInt(`0x${beginCell().storeUint(Date.now(), 64).endCell().hash().toString('hex')}`);
     const masterchefAddress = await kitchen.getGetJettonMasterChefAddress(provider.sender().address!!, seed);
-    const totalReward = toNano("0.1")
+    const totalReward = toNano("2")
     let sentAmount = (totalReward * 1003n) / 1000n;
     const deadline = BigInt(Math.floor(Date.now() / 1000) + 60 * 60);
     const startTime = BigInt(Math.floor(Date.now() / 1000)) + 10n;
