9f834deae4340e13ba575a2b2f04fac11174504e
alan890104 <alan890104@gmail.com>
2024-05-21 12:54:13 +0800

    add prompt to prevent from error

--- scripts/setWeight.ts
+++ scripts/setWeight.ts
@@ -2,27 +2,29 @@ import { toNano, Address, beginCell } from '@ton/core';
 import { JettonMasterChef } from '../wrappers/JettonMasterChef';
 import { NetworkProvider } from '@ton/blueprint';
 import { loadDeployment } from '../utils/helper';
+import { JettonMinter } from '../wrappers/JettonMinter';
+import { JettonWallet } from '../wrappers/RealJettonWallet';
 
 export async function run(provider: NetworkProvider) {
     const deployment = await loadDeployment();
 
     const masterchef = provider.open(JettonMasterChef.fromAddress(Address.parse(deployment.MasterChef)));
     console.log('MasterChef address:', masterchef.address.toString());
+
     const lpJettonWalletAddress = await provider
         .ui()
-        .inputAddress('Please enter the LP Jetton Wallet address of Masterchef for adding pool:');
-    const allocPoint = await provider.ui().input('Please enter the allocPoint:');
+        .inputAddress('Please enter the LP Jetton Wallet address of Masterchef for setting weight:');
+    const newAllocPoint = await provider.ui().input('Please enter the new allocPoint:');
 
     await masterchef.send(
         provider.sender(),
         {
-            value: toNano('0.8'),
+            value: toNano('0.08'),
         },
         {
-            $$type: 'AddPool',
+            $$type: 'Set',
             lpTokenAddress: lpJettonWalletAddress,
-            allocPoint: BigInt(allocPoint),
-            queryId: 10n,
+            allocPoint: BigInt(newAllocPoint),
         },
     );
 }
